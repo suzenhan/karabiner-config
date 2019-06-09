72a540b3fbb171ee05f81974f4aeeabad78a3ddb
suhan03 <suhan@ithan.me>
2019-06-09 20:43:20 +0800

    支持多个键盘

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cat ee6c.yml | yaml2json | jq . > ee6c.json
-./update_karabiner.py --name ee6c
-mv ee6c.json ~/.config/karabiner/assets/complex_modifications
+cat two_fn.yml | yaml2json | jq . > two_fn.json
+cat two_fn_map.yml | yaml2json | jq . > two_fn_map.json
+./update_karabiner.py --name two_fn --map-name two_fn_map
+mv two_fn.json ~/.config/karabiner/assets/complex_modifications
+mv two_fn_map.json ~/.config/karabiner/assets/complex_modifications
+
--- update_karabiner.py
+++ update_karabiner.py
@@ -11,13 +11,14 @@ karabiner_config_file = f'{Path.home()}/.config/karabiner/karabiner.json'
 
 @click.command()
 @click.option('--name', help='complex rule title', required=True)
-def main(name):
+@click.option('--map-name', help='two fn 映射', required=True)
+def main(name, map_name):
     config = json.load(open(karabiner_config_file))
 
     complex_config = json.load(open(f'{name}.json'))
     target_profile = next((profile for profile in config['profiles'] if profile['name'] == name))
-    target_profile['rules'] = complex_config['rules']
-
+    map_config = json.load(open(f'{map_name}.json'))
+    target_profile['rules'] = complex_config['rules'] + map_config['rules']
     json.dump(config, open(karabiner_config_file, 'w'), indent=2)
 
 
